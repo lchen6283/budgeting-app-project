2aff3a5e6c6ada7429749dc4cfde77c9bb0837dc
lchen6283 <lchen6283@gmail.com>
2022-07-24 16:06:15 -0400

    port typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.OUR_PORT || 3003;
+const PORT = process.env.PORT || 3003;
 
 //Listen
 app.listen(PORT, () => console.log("Listening on port:", PORT));
